1d280ff4f1455c3ffccb75e8939df33af6080cbb
adishahirushan <adishahirushan@gmail.com>
2026-05-18 14:47:40 +0530

    code calculator codes

---
+++ calculatorTest.java
@@ -0,0 +1,10 @@
+public class calculatorTest{
+    public static void main(string args[]){
+        Calculator cal = new calculator();
+        if(cal.add(2,3) == 5){
+            System.out.println("Test Passed.")
+        }else{
+            System.out.println("Test failed.")
+        }
+    }
+}
