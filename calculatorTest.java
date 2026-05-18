c53f56ffb3c51f461f21f2fffe1aad4340782469
adishahirushan <adishahirushan@gmail.com>
2026-05-18 15:07:21 +0530

    update calculator things

--- calculatorTest.java
+++ calculatorTest.java
@@ -1,10 +1,35 @@
 public class calculatorTest{
     public static void main(string args[]){
         Calculator cal = new calculator();
-        if(cal.add(2,3) == 5){
-            System.out.println("Test Passed.")
-        }else{
-            System.out.println("Test failed.")
+
+        if (calc.add(2, 3) == 5) {
+            System.out.println("Add Test Passed.");
+        } else {
+            System.out.println("Add Test Failed.");
+        }
+
+        if (calc.subtract(5, 3) == 2) {
+            System.out.println("Subtract Test Passed.");
+        } else {
+            System.out.println("Subtract Test Failed.");
+        }
+
+        if (calc.multiply(4, 3) == 12) {
+            System.out.println("Multiply Test Passed.");
+        } else {
+            System.out.println("Multiply Test Failed.");
+        }
+
+        if (calc.divide(10, 2) == 5.0) {
+            System.out.println("Divide Test Passed.");
+        } else {
+            System.out.println("Divide Test Failed.");
+        }
+
+        if (calc.modulus(10, 3) == 1) {
+            System.out.println("Modulus Test Passed.");
+        } else {
+            System.out.println("Modulus Test Failed.");
         }
     }
 }
