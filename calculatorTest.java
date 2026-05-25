790f36b54f12cdb9cf726b1579eea58801927193
adishahirushan <adishahirushan@gmail.com>
2026-05-25 13:38:34 +0530

    fix bug

--- calculatorTest.java
+++ calculatorTest.java
@@ -1,4 +1,4 @@
-public class calculatorTest{
+public class CalculatorTest{
     public static void main(String args[]){
         Calculator cal = new Calculator();
         if (cal.add(2, 3) == 5) {
